91d8fd57e596c4eeb1cc7f796313a744ee516882
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-11-26 20:14:44 -0300

    criando itens com JS

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,28 @@
-alert('oi foi crregado')
+//criando elementos do HTML com JavaScript
+function criarItemCardapio(titulo, descricao, foto){
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'item-cardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
+
+    const img = document.createElement('img')
+    img.src = foto
+    img.className = 'img-item'
+
+    //adicionando os elementos na div Mãe
+    const divC = document.getElementById('cardapio')
+
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
+}
+
+//executando a função
+criarItemCardapio('Bolo de Chocolate', 'Um clássico irresistível com camadas de chocolate.', 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
