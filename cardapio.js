25f35144c8fa1532ab6d7d11cfd75cc39345c849
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-12-03 20:15:00 -0300

    adicionando a função de buscar

--- cardapio.js
+++ cardapio.js
@@ -26,3 +26,22 @@ function criarItemCardapio(titulo, descricao, foto){
 
 //executando a função
 criarItemCardapio('Bolo de Chocolate', 'Um clássico irresistível com camadas de chocolate.', 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
+
+function buscarBolos() {
+  fetch('https://confeitaria-api-2za6.onrender.com')
+    .then(response => {
+      // Verifica se a requisição foi bem-sucedida
+      if (!response.ok) {
+        throw new Error('Erro na requisição');
+      }
+      return response.json(); // Converte a resposta para JSON
+    })
+    .then(dados => {
+      console.log('Lista de bolos:', dados); // Exibe os dados no console
+    })
+    .catch(error => {
+      console.error('Erro ao buscar bolos:', error);
+    });
+}
+
+buscarBolos();
